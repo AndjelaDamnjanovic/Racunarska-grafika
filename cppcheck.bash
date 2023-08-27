8b6726d38b051e98d6e133b07b55b03c4bf2ef40
AndjelaDamnjanovic <mi19059@alas.matf.bg.ac.rs>
2023-08-27 12:05:46 +0200

    Dodat pajton i bash skript za staticku analizu koda

---
+++ cppcheck.bash
@@ -0,0 +1,1 @@
+python3 static-analysis.py
---
+++ static-analysis.py
@@ -0,0 +1,13 @@
+import sys, os, re
+import subprocess
+
+
+print ("Zapocinjem staticku analizu: \n")
+print("------------------------------------------------------------")
+for root, dirs, files in os.walk("."):
+	for filename in files:
+		ext=os.path.splitext(filename)[1]
+		if ext in [".cpp", ".hpp", ".h"]:
+			subprocess.run(["cppcheck", (os.path.join(root, filename))])
+			print("-------------------------------------------------------")
+print("Analiza je zavrsena")
