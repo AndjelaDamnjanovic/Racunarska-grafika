41cba59bbdfa5fd01e819ebb759fea72eeb015ad
AndjelaDamnjanovic <mi19059@alas.matf.bg.ac.rs>
2023-08-28 11:31:55 +0200

    Dodat .clang-tidy fajl
    Kodu je dodat .clang-tidy fajl koji opisuje zeljen stil kodiranja kada je u pitanju imenovanje promenljivih, funkcija, struktura, itd. Primenjen je na dosad postojeci kod i greske su ispravljene.

--- src/html/search/functions_0.js
+++ src/html/search/functions_0.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['loadfromfile_0',['LoadFromFile',['../structProgramState.html#a62cd48a2ce6b91991f977b17ce30d4d1',1,'ProgramState']]]
+  ['loadFromFile_0',['loadFromFile',['../structProgramState.html#a62cd48a2ce6b91991f977b17ce30d4d1',1,'ProgramState']]]
 ];
--- src/html/search/functions_1.js
+++ src/html/search/functions_1.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['savetofile_0',['SaveToFile',['../structProgramState.html#aa036d695abb2c16721d1f64cc2072f00',1,'ProgramState']]]
+  ['saveToFile_0',['saveToFile',['../structProgramState.html#aa036d695abb2c16721d1f64cc2072f00',1,'ProgramState']]]
 ];
